e77bac9ee14909e60d402642dfffa597f54a05c5
Bopp, Florian <florian.bopp@haw-hamburg.de>
2022-06-30 10:53:33 +0200

    tryout nuxt config changes to prerender all sites and no ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr: false,
+    nitro: {
+        prerender: {
+            routes: ['/', '/about', '/contact', '/success'],
+        },
+    },
     css: ['@/assets/css/styles.css'],
     build: {
         postcss: {
